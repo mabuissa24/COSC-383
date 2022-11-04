6a9f39a3e923f6f2946f59f1f5d63a8dbab0c6bc
mabuissa24 <83733261+mabuissa24@users.noreply.github.com>
2022-11-04 12:46:09 -0400

    Update TextExtractor.py

--- P4/TextExtractor.py
+++ P4/TextExtractor.py
@@ -1,37 +1,14 @@
 import sys
+import LSBExtractor as LSB
 
-import numpy as np
-import imageio.v2 as imageio
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Doesn't specify which file, where to start and how much to read.")
+
+if __name__ == "__main__":  # Reads hidden text in specified format
+    if len(sys.argv) != 2:
+        print("Doesn't specify which file")
         exit(1)
     filename = sys.argv[1]
-    start = int(sys.argv[2])
-    length = int(sys.argv[3])
-    img = imageio.imread(filename)
-    height, width, _ = img.shape
-    print("Height:", height, "Width:", width)
+    length = LSB.readAsInt(LSB.extract(filename, 0, 32))
+    text = LSB.readAsText(LSB.extract(filename, 32, length * 8))
+    print(text)
 
-    chars = []
-    roffset = start // width  # Skip as many full rows as we can
-    count = width * roffset
-    for r in range(roffset, height):
-        for c in range(width):
-            if start <= count:
-               chars.append(str(img[r,c,0] & 1))
-               chars.append(str(img[r,c,1] & 1))
-               chars.append(str(img[r,c,2] & 1))
-            if count >= start + length:
-                break
-            count += 1
-        if count >= start + length:
-            break
-    input = "".join(chars)
-    print("input is ", input)
 
-    n = 8
-    string = ""
-    for i in range(0, len(input), n):
-        string += chr(int(input[i:i+n], 2))
-    print(string)
